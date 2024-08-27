1e9701d748ae0cf0e1f3d76f849a7939f460a445
corpi <lj845854@gmail.com>
2024-08-27 15:25:11 +0900

    feat: JwtService 생성. 액세스, 리프레시 토큰 생성 및 디코드 메서드 생성

---
+++ src/main/java/com/clean_light/server/jwt/dto/UserTokenDTO.java
@@ -0,0 +1,8 @@
+package com.clean_light.server.jwt.dto;
+
+import lombok.Data;
+
+@Data
+public class UserTokenDTO {
+    private final String loginId;
+}
---
+++ src/main/java/com/clean_light/server/jwt/service/JwtService.java
@@ -0,0 +1,75 @@
+package com.clean_light.server.jwt.service;
+
+import com.clean_light.server.jwt.dto.UserTokenDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.Date;
+
+@Service
+@RequiredArgsConstructor
+@PropertySource("classpath:jwt.properties")
+public class JwtService {
+    private static final long ACCESS_EXPIRATION_TIME = Duration.ofMinutes(30).toMillis();
+    private static final long REFRESH_EXPIRATION_TIME = Duration.ofDays(1).toMillis();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Value("${secret_key}")
+    private final String SECRET_KEY;
+
+    public String generateAccessToken(UserTokenDTO userTokenDTO) throws JsonProcessingException {
+        SecretKey secretKey = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+        String json = objectMapper.writeValueAsString(userTokenDTO);
+
+        return Jwts.builder()
+                .header()
+                    .add("type", "jwt")
+                    .and()
+                .claims()
+                    .add("user", json)
+                    .issuedAt(new Date())
+                    .expiration(new Date(System.currentTimeMillis() + ACCESS_EXPIRATION_TIME))
+                .and()
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String generateRefreshToken(UserTokenDTO userTokenDTO) throws JsonProcessingException {
+        SecretKey secretKey = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+        String json = objectMapper.writeValueAsString(userTokenDTO);
+
+        return Jwts.builder()
+                .header()
+                    .add("type", "jwt")
+                    .and()
+                .claims()
+                    .add("user", json)
+                    .issuedAt(new Date())
+                    .expiration(new Date(System.currentTimeMillis() + REFRESH_EXPIRATION_TIME))
+                    .and()
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public UserTokenDTO decodeToken(String token) throws JsonProcessingException {
+        SecretKey secretKey = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+        Claims payload = Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+        String json = payload.get("user", String.class);
+
+        return objectMapper.readValue(json, UserTokenDTO.class);
+    }
+}
