53947f07ee85b2745e7ac82a37c18a1cc574c810
corpi <lj845854@gmail.com>
2024-08-29 16:26:52 +0900

    test: 유저 회원가입 테스트

--- src/test/java/com/clean_light/server/user/service/UserAuthServiceTest.java
+++ src/test/java/com/clean_light/server/user/service/UserAuthServiceTest.java
@@ -1,4 +1,43 @@
+package com.clean_light.server.user.service;
+
+import com.clean_light.server.user.domain.User;
+import com.clean_light.server.user.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class UserAuthServiceTest {
-  
+    @Autowired UserAuthService userAuthService;
+    @Autowired UserRepository userRepository;
+
+    @Test
+    @DisplayName("유저 생성 성공 테스트(값의 검증은 컨트롤러에서 시도)")
+    @Transactional
+    public void userJoin() throws Exception {
+        /* given */
+        User willJoinUser = User.builder()
+                .loginId("loginId")
+                .password("password")
+                .email("email")
+                .nickName("nickName")
+                .build();
+
+        /* when */
+        userAuthService.join(willJoinUser);
+
+        /* then */
+        User user = userRepository.findByLoginId(willJoinUser.getLoginId()).get();
+
+        assertNotNull(user);
+        assertEquals(user.getLoginId(), willJoinUser.getLoginId());
+        assertEquals(user.getPassword(), willJoinUser.getPassword());
+        assertEquals(user.getEmail(), willJoinUser.getEmail());
+        assertEquals(user.getNickName(), willJoinUser.getNickName());
+    }
 }
