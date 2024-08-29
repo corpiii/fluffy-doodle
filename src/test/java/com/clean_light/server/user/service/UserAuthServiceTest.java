47e555300c59f48e489db1147414ffa43236178a
corpi <lj845854@gmail.com>
2024-08-29 17:00:48 +0900

    test: 유저 로그인 로그아웃 테스트

--- src/test/java/com/clean_light/server/user/service/UserAuthServiceTest.java
+++ src/test/java/com/clean_light/server/user/service/UserAuthServiceTest.java
@@ -1,11 +1,14 @@
 package com.clean_light.server.user.service;
 
 import com.clean_light.server.user.domain.User;
+import com.clean_light.server.user.dto.UserAuthToken;
 import com.clean_light.server.user.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +18,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserAuthServiceTest {
     @Autowired UserAuthService userAuthService;
     @Autowired UserRepository userRepository;
+    @Autowired PasswordEncoder passwordEncoder;
+    @Autowired RedisTemplate<String, String> redisTemplate;
 
     @Test
     @DisplayName("유저 생성 성공 테스트(값의 검증은 컨트롤러에서 시도)")
@@ -30,6 +35,72 @@ class UserAuthServiceTest {
 
         /* when */
         userAuthService.join(willJoinUser);
+
+        /* then */
+        User user = userRepository.findByLoginId(willJoinUser.getLoginId()).get();
+
+        assertNotNull(user);
+        assertEquals(user.getLoginId(), willJoinUser.getLoginId());
+        assertEquals(user.getPassword(), willJoinUser.getPassword());
+        assertEquals(user.getEmail(), willJoinUser.getEmail());
+        assertEquals(user.getNickName(), willJoinUser.getNickName());
+    }
+
+    @Test
+    @DisplayName("유저 로그인 테스트")
+    @Transactional
+    public void userLogin() throws Exception {
+        /* given */
+        String loginId = "loginId";
+        String originPassword = "password";
+        String encodedPassword = passwordEncoder.encode(originPassword);
+        User willJoinUser = User.builder()
+                .loginId(loginId)
+                .password(encodedPassword)
+                .email("email")
+                .nickName("nickName")
+                .build();
+
+        userAuthService.join(willJoinUser);
+
+        /* when */
+        User willLoginUser = User.builder()
+                .loginId(loginId)
+                .password(originPassword)
+                .build();
+
+        UserAuthToken userAuthToken = userAuthService.login(willLoginUser);
+        String accessToken = userAuthToken.getAccessToken();
+        userAuthService.login(willLoginUser);
+
+        /* then */
+        String s = redisTemplate.opsForValue().get(loginId);
+        System.out.println(s);
+    }
+
+    @Test
+    @DisplayName("유저 로그아웃 테스트")
+    @Transactional
+    public void userLogout() throws Exception {
+        /* given */
+        User willJoinUser = User.builder()
+                .loginId("loginId")
+                .password("password")
+                .email("email")
+                .nickName("nickName")
+                .build();
+
+        User willLoginUser = User.builder()
+                .loginId("loginId")
+                .password("password")
+                .build();
+
+        userAuthService.join(willJoinUser);
+        UserAuthToken userAuthToken = userAuthService.login(willLoginUser);
+        String accessToken = userAuthToken.getAccessToken();
+
+        /* when */
+        userAuthService.logout(userAuthToken.getAccessToken());
 
         /* then */
         User user = userRepository.findByLoginId(willJoinUser.getLoginId()).get();
