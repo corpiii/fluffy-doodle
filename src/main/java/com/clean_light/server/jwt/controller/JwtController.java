fc5ee69e4fc58310ab77805dca21e35e6c04aa0a
corpi <lj845854@gmail.com>
2024-09-28 18:16:07 +0900

    chore: 불필요한 import 제거 커밋 누락

--- src/main/java/com/clean_light/server/jwt/controller/JwtController.java
+++ src/main/java/com/clean_light/server/jwt/controller/JwtController.java
@@ -1,6 +1,5 @@
 package com.clean_light.server.jwt.controller;
 
-import com.clean_light.server.jwt.dto.UserTokenInfo;
 import com.clean_light.server.jwt.service.JwtService;
 import com.clean_light.server.user.dto.UserAuthToken;
 import com.clean_light.server.user.service.UserAuthService;
@@ -8,7 +7,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@Transactional
 @RequestMapping("/jwt")
 public class JwtController {
     private final JwtService jwtService;
--- src/main/java/com/clean_light/server/user/service/UserAuthService.java
+++ src/main/java/com/clean_light/server/user/service/UserAuthService.java
@@ -11,7 +11,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
