3cdc840d154d75fe3da6089c25872d08c79001a6
corpi <lj845854@gmail.com>
2025-01-03 12:24:41 +0900

    feat: ProductRepository 생성

---
+++ src/main/java/com/clean_light/server/product/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.clean_light.server.product.repository;
+
+import com.clean_light.server.product.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
