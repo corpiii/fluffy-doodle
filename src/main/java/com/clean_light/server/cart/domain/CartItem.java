5d954936a4c13e8faf30843db871795eb49bcd56
corpi <lj845854@gmail.com>
2025-01-07 16:13:08 +0900

    feat: CartItem 엔티티 생성

---
+++ src/main/java/com/clean_light/server/cart/domain/CartItem.java
@@ -0,0 +1,31 @@
+package com.clean_light.server.auth.user.domain;
+
+import com.clean_light.server.product.domain.Product;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class CartItem {
+    @Id
+    @GeneratedValue
+    @Column(name = "cart-item_id")
+    private Long id;
+
+    @OneToOne
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User oner;
+
+    private int price;
+
+    private int amount;
+
+    private int discount;
+}
