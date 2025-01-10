cebf982dbb3f5a75f731027ce96a769ef115ec73
corpi <lj845854@gmail.com>
2025-01-11 08:55:43 +0900

    feat: 장바구니 상품 추가 기능 구현

--- src/main/java/com/clean_light/server/cart/service/CartService.java
+++ src/main/java/com/clean_light/server/cart/service/CartService.java
@@ -7,6 +7,8 @@ import com.clean_light.server.auth.user.domain.User;
 import com.clean_light.server.auth.user.repository.UserRepository;
 import com.clean_light.server.auth.user.service.UserAuthService;
 import com.clean_light.server.cart.domain.CartItem;
+import com.clean_light.server.product.domain.Product;
+import com.clean_light.server.product.service.ProductService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -17,8 +19,23 @@ import org.springframework.stereotype.Service;
 public class CartService {
     private final UserRepository userRepository;
     private final JwtService jwtService;
+    private final ProductService productService;
 
     public List<CartItem> fetchCartItemListBy(String accessToken) throws JsonProcessingException {
+        User user = findUserByAccessToken(accessToken);
+
+        return user.getCartItemList();
+    }
+
+    public void addCartItem(String accessToken, Long productId) throws JsonProcessingException {
+        User user = findUserByAccessToken(accessToken);
+        Product targetProduct = productService.search(productId);
+        CartItem cartItem = new CartItem(null, targetProduct, user, targetProduct.getPrice(), 1, 0);
+
+        user.getCartItemList().add(cartItem);
+    }
+
+    private User findUserByAccessToken(String accessToken) throws JsonProcessingException {
         UserTokenInfo userTokenInfo = jwtService.decodeToken(accessToken);
         String loginId = userTokenInfo.getLoginId();
 
@@ -26,8 +43,7 @@ public class CartService {
             throw new IllegalArgumentException("해당 유저를 찾을 수 없습니다. 다시 로그인 해주세요.");
         }
 
-        User user = userRepository.findByLoginId(loginId)
+        return userRepository.findByLoginId(loginId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 유저를 찾을 수 없습니다. 다시 로그인 해주세요."));
-        return user.getCartItemList();
     }
 }
