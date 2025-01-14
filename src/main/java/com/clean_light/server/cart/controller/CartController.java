0571df3ca5468a08ddb61ff3df46553b488e071d
corpi <lj845854@gmail.com>
2025-01-14 11:49:15 +0900

    feat: 장바구니 Controller 상품 추가 기능 구현

--- src/main/java/com/clean_light/server/cart/controller/CartController.java
+++ src/main/java/com/clean_light/server/cart/controller/CartController.java
@@ -9,19 +9,23 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(value = "/api/cart")
 public class CartController {
     private final CartService cartService;
 
-    @RequestMapping(value = "/api/user/cart", method = RequestMethod.HEAD)
+    @GetMapping("/")
     public ResponseEntity<ApiResponse<List<CartItemResponse>>> fetchCartItemList(HttpServletRequest request) {
         try {
-            String accessToken = request.getParameter(HttpHeaders.AUTHORIZATION).substring(7);
+            String accessToken = request.getHeader(HttpHeaders.AUTHORIZATION).substring(7);
             List<CartItem> cartItemList = cartService.fetchCartItemListBy(accessToken);
             List<CartItemResponse> responseList = cartItemList.stream().map(CartItemResponse::new).toList();
 
@@ -32,4 +36,19 @@ public class CartController {
         }
     }
 
+    @PostMapping("/add/{productId}")
+    public ResponseEntity<ApiResponse<List<CartItemResponse>>> addCartItem(
+            HttpServletRequest request,
+            @PathVariable("productId") Long productId
+    ) {
+        try {
+            String accessToken = request.getHeader(HttpHeaders.AUTHORIZATION).substring(7);
+            cartService.addCartItem(accessToken, productId);
+
+            return ResponseEntity.ok(new ApiResponse<>(true, null, "장바구니에 추가되었습니다."));
+        } catch (Exception exception) {
+            return ResponseEntity.badRequest()
+                    .body(new ApiResponse<>(false, null, exception.getMessage()));
+        }
+    }
 }
